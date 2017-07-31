4fccf0b86c6afb19a082a025b199c0705b8b6b88
krishna <krishna@krishnas-MacBook-Air.local>
2017-08-01 00:31:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/krishna/newmewok/Word.java
@@ -0,0 +1,50 @@
+package com.example.krishna.newmewok;
+
+
+
+public class Word {
+
+    private String mDefaultTranslation;
+
+    private String mMiwokTranslation;
+
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    private static final int NO_IMAGE_PROVIDED = -1;
+    private int mAudioResourceId;
+
+    public Word(String defaultTranslation, String miwokTranslation,int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = audioResourceId;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId,int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
+    }
+
+    /**
+     * Get the default translation of the word.
+     */
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+
+    public Boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+    public int getAudioResourceId(){return mAudioResourceId;}
+}
